b1bfe83356f7b6150e10824137aebd486bfb318d
MariaFernandaMelliana <202020432@mhs.udb.ac.id>
2022-08-16 17:32:03 +0700

    ubah

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -14,7 +14,7 @@ const AddUser = () => {
   const saveUser = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:5000/users", {
+      await axios.post("https://backend-uasmaria-nodejs.herokuapp.com/users", {
         nama,
         nim,
         kelas,
